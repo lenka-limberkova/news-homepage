d1fdd15aa0535d9218dc23f3445209ccdc547dc4
Lenka Limberková <limberkova.l@gmail.com>
2026-08-25 11:42:25 +0200

    uprava css pro mobil

--- menu.js
+++ menu.js
@@ -1,13 +1,15 @@
 const hamburger = document.querySelector(".hamburger")
 const mobileNav = document.querySelector(".mobile-nav")
 const closeMenu = document.querySelector(".close-menu")
+const blackBlur = document.querySelector(".blur")
 
 // Otevreni menu
 
 hamburger.addEventListener("click", function () {
     mobileNav.classList.add("open");
-
+    
 } )
+
 
 // Zavreni menu
 

82203fa7da6af30cbfe5cfe40e40e0f18c5dba5d
Lenka Limberková <limberkova.l@gmail.com>
2026-08-25 12:33:42 +0200

    pridani blur pri vyjeti menu

--- menu.js
+++ menu.js
@@ -1,19 +1,28 @@
-const hamburger = document.querySelector(".hamburger")
-const mobileNav = document.querySelector(".mobile-nav")
-const closeMenu = document.querySelector(".close-menu")
-const blackBlur = document.querySelector(".blur")
+const hamburger = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+const closeMenu = document.querySelector(".close-menu");
+const blackBlur = document.querySelector(".blur");
+
 
 // Otevreni menu
 
 hamburger.addEventListener("click", function () {
     mobileNav.classList.add("open");
-    
-} )
+    blackBlur.classList.add("open");
+});
 
 
-// Zavreni menu
+// Zavreni menu pomoci X
 
-closeMenu.addEventListener("click", function(){
-    mobileNav.classList.remove("open")
-}
-)
+closeMenu.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    blackBlur.classList.remove("open");
+});
+
+
+// Zavreni menu kliknutim na zatmavenou cast
+
+blackBlur.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    blackBlur.classList.remove("open");
+});
